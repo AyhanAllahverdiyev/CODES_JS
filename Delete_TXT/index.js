851fac4e19f0769a98e32d233f16380a982c6dd4
AyhanAllahverdiyev <aykhanallahverdizade2020@gmail.com>
2023-06-16 19:36:37 +0300

    FILE MANIPULATION

--- Delete_TXT/index.js
+++ Delete_TXT/index.js
@@ -1,21 +1,62 @@
+//THIS CODE READS,WRITES,ADDS AND DELETES TASKS FROM A TXT FILE CALLED "INDEX.TXT"
+
 const http = require("http");
 const fs = require("fs");
 const axios = require("axios");
-const fs = require("fs");
+require("dotenv").config();
 const readlinesync = require("readline-sync");
-const { error } = require("console");
-const filepath = "tasks.txt";
+const filepath = process.env.filepath;
 
-function readfilesync(filepath) {
+function ReadFile(filepath) {
   try {
-    const filedata = fs.readfilesync(filepath, "utf8");
-    return tasksData.split("\n").filter(Boolean);
-  } catch (errror) {
+    const filedata = fs.readFileSync(filepath, "utf8");
+
+    // return filedata.split("\n").filter(Boolean);
+    return filedata.split("\n").filter(Boolean);
+  } catch (error) {
     console.log("Somethin went wrong", error);
   }
-  function display() {
-    const task = readfilesync(filepath);
-    console.log("TASKS:");
-    console.log(``);
+}
+// function  updateindex(){
+//     const tasks=ReadFile(filepath);
+//     const upindex=tasks.map()
+// }
+function display() {
+  const tasks = ReadFile(filepath);
+  console.log("TASKS:");
+  tasks.forEach((task, index) => {
+    console.log(`task: ${index + 1} ==> ${task}`);
+    console.log("\n");
+  });
+}
+function add() {
+  const NewTask = readlinesync.question("PLEASE INSERT THE NEW DATA:");
+  const tasks = ReadFile(filepath);
+  tasks.push(NewTask);
+  fs.writeFileSync(filepath, tasks.join("\n"));
+}
+
+function Delete() {
+  try {
+    const index = readlinesync.question("Insert the index number:") - 1;
+    const tasks = ReadFile(filepath);
+    if (index < tasks.length && index >= 0) {
+      const NewTask = tasks.splice(index, 1)[0];
+      // const deletedTask = tasks.splice(taskIndex, 1)[0];
+      fs.writeFileSync(filepath, tasks.join("\n"));
+      console.log(`task:${index} deleted \n`);
+    }
+  } catch (error) {
+    console.log("Error", error);
   }
 }
+
+http
+  .createServer((request, response) => {
+    console.log("Port running\n");
+    response.setHeader("Content-Type", "application/json");
+    response.write("Port running\n");
+    Delete();
+    display();
+  })
+  .listen(7000);
